3095231a0c8b4645bcf500d9bf03d2d5803dbcb5
KyungIn Lee <jess.lee2906@gmail.com>
2022-03-14 11:49:48 +1100

    Debugging attempt for issue with progress bar and choices text not loading

--- quiz assets/quiz.js
+++ quiz assets/quiz.js
@@ -4,10 +4,10 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
@@ -64,13 +64,14 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore',score)
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
         return window.location.assign('/end.html')
     }
-    questionCounter ++
+
+    questionCounter++
     progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
-    progressBarFull.getElementsByClassName.width = '${questionCounter/(MAX_QUESTIONS) * 100%}'
+    progressBarFull.style.width = '${(questionCounter/(MAX_QUESTIONS) * 100}%'
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion =   availableQuestions[questionsIndex]
@@ -111,3 +112,10 @@ choices.forEach(choice => {
     })
 
 })
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame()
